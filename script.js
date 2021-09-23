42c469b5bc0d1ad5b0df0434021c5c9aec1e83e5
cristianh <crishernandez08@hotmail.com>
2021-09-23 08:26:30 -0500

    Subiendo figuras

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
 
-let sliders = ['slide-1', 'slide-2', 'slide-3','slide-4','slide-5']
-let audios_sond = ['Audio1', 'paralelogramo1', 'paralelogramo2','paralelogramo3','paralelogramo4']
+let sliders = ['slide-1', 'slide-2', 'slide-3', 'slide-4', 'slide-5', 'slide-6']
+let audios_sond = ['Audio1', 'paralelogramo1', 'paralelogramo2', 'paralelogramo3', 'paralelogramo4']
 
 let presentacion_slide = 0
 let audio;
 let audioOvers;
 let audiosBotonesOver;
 let path_sound = './assets/sounds/'
-let videoEscena1=document.getElementById('videoescena1')
-let videoEscena2=document.getElementById('videoescena2')
-let videoEscena3=document.getElementById('videoescena3')
-let videoEscena4=document.getElementById('videoescena4')
+let videoEscena1 = document.getElementById('videoescena1')
+let videoEscena2 = document.getElementById('videoescena2')
+let videoEscena3 = document.getElementById('videoescena3')
+let videoEscena4 = document.getElementById('videoescena4')
 
-let videoEscenas=[videoEscena1,videoEscena2,videoEscena3,videoEscena4]
+let videoEscenas = [videoEscena1, videoEscena2, videoEscena3, videoEscena4]
 
 let numerador = false;
 let denominador = false;
@@ -77,13 +77,13 @@ function cargarAudio(loop = false) {
 		audio = new Audio(`${path_sound}${audios_sond[presentacion_slide]}.mp3`);
 		audio.loop = loop
 		audio.play();
-        audio.onloadeddata = function() {
-            
-            
-        };
-        audio.addEventListener('ended', function () {
-            
-        });
+		audio.onloadeddata = function () {
+
+
+		};
+		audio.addEventListener('ended', function () {
+
+		});
 	}
 
 }
@@ -94,8 +94,8 @@ function changeSound(new_sond) {
 	audio.load();
 	audio.play();
 	audio.addEventListener('canplaythrough', function () {
-        /* document.getElementById('contenedor-explicacion-paralelogramo') */
-		videoEscenas[getCurrentSlider()-1].play() 
+		/* document.getElementById('contenedor-explicacion-paralelogramo') */
+		videoEscenas[getCurrentSlider() - 1].play()
 	});
 }
 
@@ -109,15 +109,15 @@ function presentacion() {
 	} else if (presentacion_slide == sliders.length - 1) {
 		document.getElementById('siguiente').style.display = "none"
 		//document.getElementById('atras').style.display = "none"
-        document.getElementById('atras').style.display = "inline-block"
+		document.getElementById('atras').style.display = "inline-block"
 		document.getElementById('atras').style.visibility = "visible"
 		document.getElementById('siguiente').style.display = "none"
-		
-	}  else {
+
+	} else {
 		document.getElementById('siguiente').style.display = "inline-block"
 		document.getElementById('atras').style.visibility = "visible"
-        document.getElementById('siguiente').style.visibility = "visible"
-		
+		document.getElementById('siguiente').style.visibility = "visible"
+
 		/* document.getElementById('actividad').style.display = "none" */
 
 	}
@@ -133,7 +133,7 @@ function siguiente() {
 		document.getElementById(sliders[presentacion_slide]).style.display = "block"
 		audioOvers = new Audio(`${path_sound}60.mp3`);
 		audioOvers.play();
-        reproducirvideosEscenas()
+		reproducirvideosEscenas()
 		changeSound(audios_sond[presentacion_slide])
 		presentacion()
 	}
@@ -145,7 +145,7 @@ function atras() {
 	document.getElementById(sliders[presentacion_slide]).style.display = "block"
 	audioOvers = new Audio(`${path_sound}60.mp3`);
 	audioOvers.play();
-    reproducirvideosEscenas()
+	reproducirvideosEscenas()
 	changeSound(audios_sond[presentacion_slide])
 	presentacion()
 }
@@ -154,17 +154,55 @@ function getCurrentSlider() {
 	return presentacion_slide
 }
 
-function cortar(){
-    document.getElementById('ejemplo').style.animation = 'mymove 6s forwards'
+function cortar(posicion) {
+	switch (posicion) {
+		case 1:
+			gsap.to('#figura1B', {
+				duration: 3,
+				x:129,
+				ease:Back.easeOut
+			  })
+			break;
+		case 2:
+			
+			gsap.to('#figura2A', {
+				duration: 3,
+				x:240,
+				ease:Back.easeOut
+			  })
+			break;
+		case 3:
+			
+			
+			gsap.to('#figura3A', {
+				duration: 3,
+				x: 213,
+				ease:Back.easeOut
+			  })
+			break;
+		case 4:
+			
+			gsap.to('#figura4A', {
+				duration: 3,
+				x: 190,
+				ease:Back.easeOut
+			  })
+			
+			break;
+
+		default:
+			break;
+	}
+
 }
 
-function reproducirvideosEscenas(){
-    stopAllVideos()
-    //videoEscenas[getCurrentSlider()-1].play() 
+function reproducirvideosEscenas() {
+	stopAllVideos()
+	//videoEscenas[getCurrentSlider()-1].play() 
 }
 
-function stopAllVideos(){
-    videoEscenas.forEach(element => {
-        element.pause()
-    });
+function stopAllVideos() {
+	videoEscenas.forEach(element => {
+		element.pause()
+	});
 }
